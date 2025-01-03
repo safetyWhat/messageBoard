a3888e056fb47ce42a6eb8ee88e6dd6c7f173b4f
safetyWhat <137813061+safetyWhat@users.noreply.github.com>
2025-01-03 17:24:52 -0500

    Remove dateset function from the indexRouter

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,15 +2,6 @@ const { Router } = require("express");
 const indexRouter = Router();
 const indexController = require('../controllers/indexController');
 
-const newDate = () => {
-    const date = new Date;
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-
-    return `${month}/${day}/${year}`;
-} 
-
 indexRouter.get("/", indexController.getMessages);
 indexRouter.get('/new', indexController.newMessageGet);
 indexRouter.post('/new', indexController.newMessagePost);
